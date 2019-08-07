692aef6a29f55a68265702a56dec29c0073bd5e3
Mike <mikehammond32@gmail.com>
2019-08-06 22:27:52 -0400

    created index and create backend actions

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import BooksNew from './views/BooksNew.vue';
 
 Vue.use(Router)
 
@@ -12,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/books/new',
+      name: 'books-new',
+      component: BooksNew
+
     },
     {
       path: '/about',
